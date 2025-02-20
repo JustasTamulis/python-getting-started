8ef858e39950ee3d5fd4b626669f42c405acc9b1
Justas T <justas.tam@gmail.com>
2025-02-20 16:02:55 +0200

    uncommenting posgress

--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,5 @@
 import requests
+import os
 from django.http import HttpResponse
 
 from django.shortcuts import render
@@ -8,9 +9,13 @@ from .models import Greeting
 # Create your views here.
 
 
+# def index(request):
+#     r = requests.get('https://httpbin.org/status/418', timeout=10)
+#     return HttpResponse('<pre>' + r.text + '</pre>')
+
 def index(request):
-    r = requests.get('https://httpbin.org/status/418', timeout=10)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    times = int(os.environ.get('TIMES', 3))
+    return HttpResponse('Hello! ' * times)
 
 
 
