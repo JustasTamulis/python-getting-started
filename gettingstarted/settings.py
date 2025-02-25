d95df0aff1ecb1c36e80369c6aba8b6f7f0ab55a
Justas T <justas.tam@gmail.com>
2025-02-25 12:29:41 +0200

    fix3 admin

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -87,10 +87,10 @@ INSTALLED_APPS = [
     # Use WhiteNoise's runserver implementation instead of the Django default, for dev-prod parity.
     "whitenoise.runserver_nostatic",
     "django.contrib.admin",
-    # "django.contrib.auth",
+    "django.contrib.auth",
     "django.contrib.contenttypes",
-    # "django.contrib.sessions",
-    # "django.contrib.messages",
+    "django.contrib.sessions",
+    "django.contrib.messages",
     "django.contrib.staticfiles",
     "hello",
 ]
