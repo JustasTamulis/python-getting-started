d74bf955dc8264e37be7e0b43834d5859e625d93
Justas T <justas.tam@gmail.com>
2025-02-26 12:14:01 +0200

    include frontend by vite with basic pages as components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import {Routes, Route} from 'react-router'
+import './App.css'
+import Home from './components/Home'
+import Create from './components/Create'
+import Edit from './components/Edit'
+import Delete from './components/Delete'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/delete/:id" element={<Delete />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
